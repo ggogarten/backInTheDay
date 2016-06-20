dd08c0bd76df0277657c348594f13cdf448e80b6
George G <ggogarten@gmail.com>
2016-06-20 18:26:36 -0400

    working scrub bar, last item still missing is the music minutes/seconds counter.

--- Back In The Day/ViewController.swift
+++ Back In The Day/ViewController.swift
@@ -7,12 +7,99 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
 
+    
+    var player: AVAudioPlayer = AVAudioPlayer()
+    
+    
+    @IBAction func play(sender: AnyObject) {
+       
+        
+        player.play()
+        
+        
+    }
+    
+    
+    @IBOutlet weak var volume: UISlider!
+    
+    @IBAction func adjustVolume(sender: AnyObject) {
+        
+        player.volume = volume.value
+        
+        
+        
+        
+    }
+    
+    @IBOutlet weak var scrubSlider: UISlider!
+    
+    @IBAction func scrub(sender: AnyObject) {
+        
+        player.currentTime = NSTimeInterval(scrubSlider.value)
+        
+        
+        
+        
+    }
+    
+    
+    @IBAction func stop(sender: AnyObject) {
+     
+        player.pause()
+        
+        do {
+            
+            try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("backInTheDay", ofType: "mp3")!))
+            
+            scrubSlider.maximumValue = Float(player.duration)
+            
+        } catch {
+            
+            print("It didn't work")
+            
+        }
+        
+    }
+    
+    
+    @IBAction func pause(sender: AnyObject) {
+        
+        player.pause()
+        
+        
+    }
+    
+    func updateScrubSlider() {
+        
+        scrubSlider.value = Float(player.currentTime)
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        do {
+        
+       try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("backInTheDay", ofType: "mp3")!))
+            
+            scrubSlider.maximumValue = Float(player.duration)
+            
+        } catch {
+            
+            print("It didn't work")
+            
+        }
+        
+        var timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("updateScrubSlider"), userInfo: nil, repeats: true)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
